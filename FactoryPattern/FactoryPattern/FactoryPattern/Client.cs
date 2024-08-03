1117ae487a327f2159b08b9a5dbe6d51a9aaff24
volkantabanli-infoline <volkan.tabanli@infoline-tr.com>
2024-08-03 17:31:33 +0300

    Factory Pattern eklendi.

---
+++ FactoryPattern/FactoryPattern/FactoryPattern/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class Client
+    {
+        public void Main()
+        {
+            IVehicleFactory carFactory = new CarFactory();
+            IVehicle car = carFactory.CreateVehicle();
+            Console.WriteLine(car.Drive());
+
+            IVehicleFactory truckFactory = new TruckFactory();
+            IVehicle truck = truckFactory.CreateVehicle();
+            Console.WriteLine(truck.Drive());
+        }
+    }
+}
---
+++ FactoryPattern/FactoryPattern/FactoryPattern/Truck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class Truck : IVehicle
+    {
+        public string Drive()
+        {
+            return "Driving a truck!";
+        }
+    }
+}
